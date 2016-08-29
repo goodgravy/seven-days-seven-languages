7c387815833145bff1ac164b5f34bd99767ae346
James Brady <james@jmsbrdy.com>
2016-08-29 16:35:59 +0100

    feat: monitor restarts itself

--- erlang/day_3.erl
+++ erlang/day_3.erl
@@ -8,9 +8,18 @@ mon_translate() ->
       io:format("creating a new translation process~n"),
       register(translator, spawn_link(fun translate:loop/0)),
       mon_translate();
+    start ->
+      io:format("creating a new monitor process, linked to ~p~n", [whereis(translator)]),
+      link(whereis(translator)),
+      mon_translate();
     {'EXIT', From, Reason} ->
-      io:format("The translator ~p died: ~p~n", [From, Reason]),
-      self() ! new,
+      io:format("Something died: ~p, ~p~n", [From, Reason]),
+      case whereis(translator) of
+        undefined -> % translator died
+          self() ! new;
+        _ ->
+          self() ! start
+      end,
       mon_translate()
   end.
 
